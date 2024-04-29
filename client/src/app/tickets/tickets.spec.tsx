d3ff33e427d70a4a9997cf49134c57346a4b5571
huuquang368 <huuquang368@gmail.com>
2024-04-29 23:13:20 +0700

    add test

--- client/src/app/tickets/tickets.spec.tsx
+++ client/src/app/tickets/tickets.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import Tickets from './tickets';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -9,23 +9,19 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate, // Return an empty jest function to test whether it was called or not...I'm not depending on the results so no need to put in a return value
 }));
 
+const queryClient = new QueryClient();
 describe('Tickets', () => {
-  it('should render successfully', () => {
-    const queryClient = new QueryClient();
-
+  it('should render successfully', async () => {
     const { baseElement } = render(
       <QueryClientProvider client={queryClient}>
         <Tickets />
       </QueryClientProvider>
     );
     expect(baseElement).toBeTruthy();
-    expect(baseElement).toMatchSnapshot();
-    const ticket = screen.getByTestId('ticket-0');
-    expect(ticket).toHaveTextContent('Install a monitor arm');
-    const input = screen.getByLabelText('ticket-input');
-    fireEvent.change(input, { target: { value: 'new' } });
-    const addButton = screen.getByRole('button', { name: 'add' });
-    fireEvent.click(addButton);
-    expect(screen.getByTestId('ticket-2')).toHaveTextContent('new');
+    await waitFor(() =>
+      expect(screen.getByTestId('ticket-1')).toHaveTextContent(
+        'Install a monitor arm'
+      )
+    );
   });
 });
